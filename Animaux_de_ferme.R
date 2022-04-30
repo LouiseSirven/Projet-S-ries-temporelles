c74c276f3a87a575362f28bc0185ba96b4bc1014
LouiseSirven <22sirven.louise@gmail.com>
2022-04-30 20:03:54 +0200

    Représentations graphiques avec bon axe des abscisses

--- Animaux_de_ferme.R
+++ Animaux_de_ferme.R
@@ -1,26 +1,43 @@
 require(zoo)
 require(tseries)
 
-path <- "C:/Users/22sir/Desktop/ST/ProjetST"
-setwd(path) #dÃ©finit l'espace de travail (working directory ou "wd")
-getwd() #affiche le wd
-list.files() #liste les ?l?ments du wd
+#espace de travail
+path <- "C:/Users/22sir/Projet-Series-temporelles"
+setwd(path) 
+getwd() 
+list.files()
 
-datafile <- "valeurs_mensuelles.csv"
+#importation du fichier
+datafile <- "Animaux_ferme.csv"
+data <- read.csv(datafile,sep=";")
 
-data <- read.csv(datafile,sep=";") #importe un fichier .csv dans un objet de classe data.frame
-xm.source <- zoo(data[[2]]) #convertit le premier ?l?ment de data en s?rie temporelle de type "zoo"
-T <- length(xm.source)
-xm <- xm.source[1:(T-4)] #supprime les 4 derni?res valeurs
+#Mise en forme des dates
+data$Date<-as.Date(paste(data$Date,1,sep="-"), format = "%Y-%m-%d")
+data$annee=as.numeric(format(data$Date, format = "%Y"))
+data$mois=as.numeric(format(data$Date, format = "%m"))
+dates_char = as.character(data$Date)
+dates_char[1];dates_char[length(dates_char)] #affiche la premi`ere et la derni`ere date
+dates <- as.yearmon(seq(from=2001,to=2022+1/12,by=1/12))
 
-plot(xm) #repr?sente graphiquement xm
+#Tri de la série par date croissante
+data<-data[order(data$Date,decreasing=FALSE),] 
+xm <- zoo(data$Indice, order.by = dates)
+##T <- length(xm)
+##Xt.ts<-ts(data$Indice, start=c(2001,1), end=c(2022,2), frequency = 12)
 
-plot(xm, xaxt="n") #repr?sente xm sans l'axe des abcisses
-axis(side=1,at=seq(0,196,12)) #affiche l'axe des abcisses par 12 de 0 ? 196
-acf(xm) #trace l'autocorr?logramme total
-#serie_decomp<-decompose(xm,type="additive")
-desaison <- xm-lag(xm,-1)
-plot(desaison)
-acf(desaison)
-pacf(desaison)
-pp.test(desaison)
+#représentation graphique
+dev.off()
+par(mar=c(4,4,1,1))#ajustement des marges
+plot(xm, ylab="IPI", xlab='Date')
+
+#stationnarisation de la série
+acf(xm) #autocorrélogramme série brute
+dxm <- diff(xm,1)#série sans la tendance
+##dXt.ts <- diff(Xt.ts,1)
+plot(dxm, xlab='Date')
+dev.off()
+par(mfrow=c(2,1),mar=c(1,1,1,1)) #définit la disposition des graphiques en deux lignes et une colonne
+acf(dxm);pacf(dxm) #trace les autocorrélogrammes total et partiel
+
+#tests de stationnarité
+pp.test(dxm)
